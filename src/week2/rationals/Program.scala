881cb6c327b6b2d33901f5faaacb63bdbacd5ba9
Andrew <logan.zhi@gmail.com>
2016-10-31 01:17:30 +0200

    completed 3/4 of week3 assignment

---
+++ src/week2/rationals/Program.scala
@@ -0,0 +1,11 @@
+package week2.rationals
+
+object Program extends App {
+  val x = new Rational(1, 2)
+  val y = new Rational(1, 4)
+  val z = new Rational(3)
+
+  println(x.*(y))
+  println(x * y)
+
+}
